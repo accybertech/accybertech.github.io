f5d4d1ecda3e7ee471d1ab7566d661606dbb0e50
_zero_ <antonino.cacciottolioutlook.it>
2025-06-25 11:14:32 +0200

    Rimuovi l'immagine QR Code obsoleta e aggiorna il percorso dell'immagine; aggiungi pulsante "scroll to top" con funzionalità JavaScript; aggiungi stili per il pulsante nel CSS

---
+++ script.js
@@ -0,0 +1,16 @@
+
+//pulsante scroll to top
+  const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+  window.onscroll = function () {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      scrollTopBtn.style.display = "block";
+    } else {
+      scrollTopBtn.style.display = "none";
+    }
+  };
+
+  scrollTopBtn.onclick = function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
